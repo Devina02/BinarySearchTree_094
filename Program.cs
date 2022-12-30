130672ddd1de0e2fa8dcaf5b5d0e58dba01dc2a3
Devina02 <devina.fitriapngs@gmail.com>
2022-12-30 17:14:25 +0700

    membuat public node

--- Program.cs
+++ Program.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_094
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
